a7cabc5292b09fcd48f12c1100433ba36c9b49f3
Rafael Sulimann <sulimannrafael@gmail.com>
2022-03-02 15:53:06 -0300

    Project Created

---
+++ src/test/java/com/sulimann/springpostgresqldocker/SpringPostgresqlDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulimann.springpostgresqldocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresqlDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
